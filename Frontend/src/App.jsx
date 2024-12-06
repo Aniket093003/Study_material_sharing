176697d4f217f8aedddf57c555e5e2cdb42855b0
Aniket093003 <aniketkainth98105@gmail.com>
2024-12-07 01:34:18 +0530

    frontend com

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./home/Home";
-
+import SignUp from "./Screens/Signup";
+import SignIn from "./Screens/SignIn";
 const App = () => {
+  
   return (
     <Router>
       <div className="app">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/SignIn" element={<SignIn />} />
+
         </Routes>
       </div>
     </Router>
--- Frontend/src/home/Home.jsx
+++ Frontend/src/home/Home.jsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
-import HeroSection from "../components/HeroSection";
 import CategoryList from "../components/CategoryList";
 import Footer from "../components/Footer";
+import Material from "../components/Material";
+import SignUp from "../Screens/SignUp";
+import SignIn from "../Screens/SignIn";
+import TopSection from "../components/TopSection";
 
 const HomePage = () => {
+  const [showSignUpModal, setShowSignUpModal] = useState(false);
+  const [showSignInModal, setShowSignInModal] = useState(false); 
+
+  const openSignUpModal = () => {
+    setShowSignUpModal(true); 
+  };
+
+  const closeSignUpModal = () => {
+    setShowSignUpModal(false); 
+  };
+
+  const openSignInModal = () => {
+    setShowSignInModal(true); 
+  };
+
+  const closeSignInModal = () => {
+    setShowSignInModal(false); 
+  };
+
   return (
     <div>
-      <Navbar />
-      <HeroSection />
+      <Navbar openSignUpModal={openSignUpModal} openSignInModal={openSignInModal} />
+      <TopSection />
       <CategoryList />
+      <Material />
       <Footer />
+
+      {showSignUpModal && <SignUp closeModal={closeSignUpModal} />}
+
+      {showSignInModal && <SignIn closeModal={closeSignInModal} />}
     </div>
   );
 };
