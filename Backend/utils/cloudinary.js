a4cd5e81a26cb1581786b8692024da4507f6634f
Aniket093003 <aniketkainth98105@gmail.com>
2024-12-08 01:12:45 +0530

    added more specs

---
+++ Backend/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: 'dvhupcbtp', 
+    api_key: '649228874784636', 
+    api_secret: 'FlF4HBL79LgQ3P45Z1NUOaZtxtM' // Click 'View API Keys' above to copy your API secret
+});
