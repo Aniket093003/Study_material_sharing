77ada65464687e2389e28b5a8c3b4de7b18e9261
Aniket093003 <aniketkainth98105@gmail.com>
2024-12-07 20:09:40 +0530

    signout handler

--- Frontend/src/home/Home.jsx
+++ Frontend/src/home/Home.jsx
@@ -8,36 +8,53 @@ import SignIn from "../Screens/SignIn";
 import TopSection from "../components/TopSection";
 
 const HomePage = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!localStorage.getItem("userToken") // Check token in localStorage
+  );
   const [showSignUpModal, setShowSignUpModal] = useState(false);
-  const [showSignInModal, setShowSignInModal] = useState(false); 
+  const [showSignInModal, setShowSignInModal] = useState(false);
 
   const openSignUpModal = () => {
-    setShowSignUpModal(true); 
+    setShowSignUpModal(true);
   };
 
   const closeSignUpModal = () => {
-    setShowSignUpModal(false); 
+    setShowSignUpModal(false);
   };
 
   const openSignInModal = () => {
-    setShowSignInModal(true); 
+    setShowSignInModal(true);
   };
 
   const closeSignInModal = () => {
-    setShowSignInModal(false); 
+    setShowSignInModal(false);
+  };
+
+  const handleLogin = (token) => {
+    localStorage.setItem("userToken", token); // Store token
+    setIsAuthenticated(true); // Update auth state
+    closeSignInModal(); // Close the modal
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("userToken"); // Remove token
+    setIsAuthenticated(false); // Update auth state
   };
 
   return (
     <div>
-      <Navbar openSignUpModal={openSignUpModal} openSignInModal={openSignInModal} />
+      <Navbar
+        isAuthenticated={isAuthenticated}
+        openSignUpModal={openSignUpModal}
+        openSignInModal={openSignInModal}
+        handleLogout={handleLogout}
+      />
       <TopSection />
       <CategoryList />
       <Material />
       <Footer />
-
       {showSignUpModal && <SignUp closeModal={closeSignUpModal} />}
-
-      {showSignInModal && <SignIn closeModal={closeSignInModal} />}
+      {showSignInModal && <SignIn handleLogin={handleLogin} closeModal={closeSignInModal} />}
     </div>
   );
 };
