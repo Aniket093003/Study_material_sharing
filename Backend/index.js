c39c60bb56db5ae7ba6e3e9f7377ce62baae15d7
Aniket093003 <aniketkainth98105@gmail.com>
2024-12-08 01:13:00 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
 import connectDB from "./db/index.js";
 import userRouter from "./routes/user.routes.js";
 import materialRouter from "./routes/material.route.js";
 connectDB();
 import cors from "cors";
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
-app.use(cors({
-  origin: "http://localhost:5173", // Frontend URL
-  methods: ["GET", "POST"],
-}));
-app.use(express.json());
+app.use(express.json({ limit: "32kb" }));
+app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/material", materialRouter);
