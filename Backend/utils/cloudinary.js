c5ced01ec911e1e721309fc109dbe7a90e4ae590
Aniket093003 <aniketkainth98105@gmail.com>
2024-12-09 21:12:18 +0530

    error handlers

--- Backend/utils/cloudinary.js
+++ Backend/utils/cloudinary.js
@@ -1,8 +1,31 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs"
+import dotenv from 'dotenv';
+dotenv.config();
 
 cloudinary.config({ 
-    cloud_name: 'dvhupcbtp', 
-    api_key: '', 
-    api_secret: '' // Click 'View API Keys' above to copy your API secret
+    cloud_name: process.env.cloud_name, 
+    api_key: process.env.api_key, 
+    api_secret: process.env.api_secret
 });
+
+import cloudinary from 'cloudinary';
+
+const uploadImage = async (filePath) => {
+  try {
+    if(!filePath) return null
+    const result = await cloudinary.uploader.upload(filePath, {
+      folder: 'Books_cover' ,
+      resource_type: "auto"
+    });
+    console.log("file uploaded", result.url);
+    return result;
+  } catch (error) {
+    fs.unlinkSync(filePath)  //remove the locally saved temp. file as uploaded opration got failed
+    console.error(error);
+  }
+};
+
+
+
+export {uploadImage}
