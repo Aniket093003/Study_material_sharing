862b6a6bb74a0e2f20378f312a98cf6a2da6eeb0
Aniket093003 <aniketkainth98105@gmail.com>
2024-12-10 19:47:38 +0530

    multar config

--- Backend/src/middleware/multerConfig.js
+++ Backend/src/middleware/multerConfig.js
@@ -4,5 +4,9 @@ import multer from "multer";
 const storage = multer.diskStorage({
     designation: function (req, file, cb){
         cb(null, "../public/temp")
-    }
+    },
+    filename: function(req, file, cb){
+    cb(null, file.originalname)}
 })
+
+export const upload = multer ({storage: storage })
