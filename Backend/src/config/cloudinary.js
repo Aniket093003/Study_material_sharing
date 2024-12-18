bc80d9ea3ead502faded83d644b9d331a271a339
Aniket093003 <aniketkainth98105@gmail.com>
2024-12-19 00:48:11 +0530

    config files

--- Backend/src/config/cloudinary.js
+++ Backend/src/config/cloudinary.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,4 +9,12 @@ cloudinary.config({
     api_secret: process.env.api_secret
 });
 
-export default cloudinary;
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: 'materials', // Folder in Cloudinary
+      resource_type: 'auto', // Supports all file types
+    },
+  });
+export default storage;
--- Backend/src/config/multerConfig.js
+++ Backend/src/config/multerConfig.js
@@ -1,19 +1,15 @@
-import multer from 'multer';
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "./cloudinary.js";
 
-const storage = multer.memoryStorage();
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'application/pdf') {
-        cb(null, true);
-    } else {
-        cb(new Error('Only PDF files are allowed'), false);
-    }
-};
-
-const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter,
-    limits: { fileSize: 2 * 1024 * 1024 } // 2MB file size limit
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "study_materials", 
+    resource_type: "auto", 
+  },
 });
+
+const upload = multer({ storage });
 
 export default upload;
