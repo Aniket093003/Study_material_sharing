f6d0d4897036ac5e925a335d1c8a8f4d31de5337
Aniket093003 <aniketkainth98105@gmail.com>
2024-12-26 01:22:01 +0530

    updated cloudniry config

--- Backend/src/config/cloudinary.js
+++ Backend/src/config/cloudinary.js
@@ -1,5 +1,4 @@
 import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,12 +8,50 @@ cloudinary.config({
     api_secret: process.env.api_secret
 });
 
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+      if (!localFilePath) return null;
+      // upload file on cloudnary
 
-const storage = new CloudinaryStorage({
-    cloudinary,
-    params: {
-      folder: 'materials', // Folder in Cloudinary
-      resource_type: 'auto', // Supports all file types
-    },
-  });
-export default storage;
+      const response = await cloudinary.uploader.upload(localFilePath, {
+          resource_type: 'auto',
+          media_metadata:true,
+          image_metadata:true,
+      })
+      // file have been uploaded successfully
+      fs.unlinkSync(localFilePath);
+      console.log("file is uploaded on cloudinary", response.url);
+      return response;
+  } catch (error) {
+      fs.unlinkSync(localFilePath); // remove locally saved temp file as the upload operation got failed
+      return null;
+  }
+
+}
+
+const deleteOnCloudinary = async (oldFileURL) => {
+  try {
+      if (!oldFileURL) return null;
+      // upload file on cloudnary
+
+      const publicId = getPublicId(oldFileURL);
+      console.log(oldFileURL, publicId);
+      if (!publicId) {
+          return null;
+      }
+      const response = await cloudinary.uploader.destroy(publicId);
+
+      console.log("file is delete on cloudinary", response);
+      return response;
+  } catch (error) {
+      return null;
+  }
+
+}
+
+const getPublicId = (url = '') => {
+  const id = url.split('/').pop().split('.')[0];
+  return id;
+}
+
+export { uploadOnCloudinary, deleteOnCloudinary };
